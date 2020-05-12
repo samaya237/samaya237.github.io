f8b4becf3f7e4acce18bafa9b506698d3f3fcca6
samaya237 <samaya.pahwa@gmail.com>
2020-05-12 19:48:32 +0530

    Check

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
 
 function printAnswer(){
 
-    function findanswer(answer){
+    function findanswer(answer,question){
         var radiob=document.getElementsByName(answer);
         var i;
         var j=0;
@@ -25,14 +25,30 @@ function printAnswer(){
             }
            
         }
+        if(j==1){
+            document.getElementById(question).style.color="#138a4b";
+        }
+       else{
+           document.getElementById(question).style.color="red"
+       }
         return j;
     }
-    var score=findanswer('answer1')+findanswer('answer2')+findanswer('answer3')+findanswer('answer4')+findanswer('answer5')+findanswer('answer6');
+    var score=findanswer('answer1','ques1')+findanswer('answer2','ques2')+findanswer('answer3','ques3')+findanswer('answer4','ques4')+findanswer('answer5','ques5')+findanswer('answer6','ques6');
     var fname=document.getElementById('box').value;
     document.getElementById('result').innerHTML='Your score is '+score;
+        if(score>3){
+            document.getElementById('result').style.color="#138a4b"
+        }
+        else{
+            document.getElementById('result').style.color="red"
+        }
     alert( 'Hello '+fname+ ' .Your score is '+score);
 }
 
 function box(){
     alert ("Welcome to my quiz")
+    var d=new Date();
+    document.getElementById('newDate').innerHTML=d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear();
+    document.getElementById('newDate').style.color="white"
 }
+
