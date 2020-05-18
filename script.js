849f481724cabdf79d123a336c75873ce7918710
samaya237 <samaya.pahwa@gmail.com>
2020-05-18 16:40:26 +0530

    Commit

--- script.js
+++ script.js
@@ -33,6 +33,32 @@ function printAnswer(){
        }
         return j;
     }
+    if(document.getElementById('q1a1').checked==false && document.getElementById('q1a2').checked==false && document.getElementById('q1a3').checked==false){
+        alert("Please complete all the questions");
+        return false;
+    }
+
+    if(document.getElementById('q2a1').checked==false && document.getElementById('q2a2').checked==false && document.getElementById('q2a3').checked==false){
+        alert("Please complete all the questions");
+        return false;
+    }
+    if(document.getElementById('q3a1').checked==false && document.getElementById('q3a2').checked==false && document.getElementById('q3a3').checked==false){
+        alert("Please complete all the questions");
+        return false;
+    } 
+    if(document.getElementById('q4a1').checked==false && document.getElementById('q4a2').checked==false && document.getElementById('q4a3').checked==false){
+        alert("Please complete all the questions");
+        return false;
+    }
+     if(document.getElementById('q5a1').checked==false && document.getElementById('q5a2').checked==false && document.getElementById('q5a3').checked==false){
+        alert("Please complete all the questions");
+        return false;
+    } 
+    if(document.getElementById('q6a1').checked==false && document.getElementById('q6a2').checked==false && document.getElementById('q6a3').checked==false){
+        alert("Please complete all the questions");
+        return false;
+    }
+
     var score=findanswer('answer1','ques1')+findanswer('answer2','ques2')+findanswer('answer3','ques3')+findanswer('answer4','ques4')+findanswer('answer5','ques5')+findanswer('answer6','ques6');
     var fname=document.getElementById('box').value;
     document.getElementById('result').innerHTML='Your score is '+score;
